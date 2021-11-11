6150bb98cdd86f5678d79638a7dbca7d19ec8a4e
Gustavo Silva <gustavosantaremsilva@gmail.com>
2021-11-11 14:57:27 +0000

    misc: removes file and line details (#19)
    Removes unnecessary file and line details from Info logging calls.
    Remains on Error for easier debug, although it might be better revise it
    some time later.

--- internal/logs/logger.go
+++ internal/logs/logger.go
@@ -33,8 +33,8 @@ type Log struct {
 // InitLogger instantiates three types of logs: An info log, a warn log and an error log.
 func InitLogger() *Log {
 	return &Log{
-		InfoLogger:  log.New(os.Stdout, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile),
-		ErrorLogger: log.New(os.Stderr, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile),
+		InfoLogger:  log.New(os.Stdout, "INFO: ", log.Ldate|log.Ltime),
+		ErrorLogger: log.New(os.Stderr, "ERROR: ", log.Ldate|log.Ltime|log.Llongfile),
 	}
 }
 
